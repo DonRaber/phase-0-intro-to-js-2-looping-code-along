1c9d9fad67cda22e456e89003de8aa9f8a1e30ab
Donovan Raber <donovan.raber@outlook.com>
2023-08-07 22:03:36 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const gifter = ["Guadalupe,", "Ollie", "Aki"];
+
+function writeCards(gifter) {
+    var messages = [];
+        for (let m = 0; m < gifter.length; m++) {
+        messages.push(`Thank you, ${gifter[m]}, for the wonderful surprise gift!`);    
+    }  
+    return messages;
+}
+
+writeCards(gifter);
+
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter--);
+    }
+}
+
+countDown();
